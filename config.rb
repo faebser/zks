c1620f1398cffdbe539963907d2233644ff6c1c0
Fabian Frei <fabian.frei@esf-frei.ch>
2014-09-16 18:50:57 +0200

    added gridle, moved scss out of root

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "static/css"
 sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "static/img"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
